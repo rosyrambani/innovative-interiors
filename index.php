31d22dc8fdf8c8a598c6033451af530e147f2efd
rosyrambani <rosys9319@gmail.com>
2017-07-13 18:05:52 -0400

    innovative interiors

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <!-- <script type="text/javascript" src="pagescroll.js"></script> -->
+	<title> Home Renovation</title>
+</head>
+<body id="page-top">
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top affix-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Innovative Interiors</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="">
+                        <a class="page-scroll" href="#download">About</a>
+                    </li>
+                    <li class="">
+                        <a class="page-scroll" href="#myCarousel">Our Work</a>
+                    </li>
+                    <li class="">
+                        <a class="page-scroll" href="#joinTeam">Join Our Team</a>
+                    </li>
+                    <li class="">
+                        <a class="page-scroll" href="#features">Services</a>
+                    </li>
+                    <li class="">
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+<header>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-7">
+                    <div class="header-content">
+                        <div class="header-content-inner">
+                            <h1>A new way to design your home .... the home you have always wanted</h1>
+                            <a id="findpro" href="#download" class="btn btn-outline btn-xl page-scroll">Find Professionals!</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-5">
+                    <div class="device-container">
+                        <div class="device-mockup iphone6_plus portrait white">
+                            <div class="device">
+                                <div class="screen">
+                                    
+                                    <img src="images/home.jpg" class="img-responsive" alt="">
+                                </div>
+                                <div class="button">
+                                    <!-- You can hook the "home button" to some JavaScript events or just remove it -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/kitchen.jpg" alt="Sample Kitchen">
+      <div class="carousel-caption">
+        <h3>Kitchen</h3>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="images/home1.jpg" alt="Sample Living Room">
+      <div class="carousel-caption">
+        <h3>Living Room</h3>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="images/home3.jpg" alt="Sample Living Room">
+      <div class="carousel-caption">
+        <h3>Living Room</h3>
+      </div>
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    <section id="download" class="download bg-primary text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2 class="section-heading">Talk to our team to transform your house to a dream home...</h2>
+                    <p>Our app Innovative Interiors is available for free download on all mobile devices</p>
+                    <div class="badges">
+                        <a class="badge-link" href="#"><img src="images/googleplay.svg" alt=""></a>
+                        <a class="badge-link" href="#"><img src="images/appstore.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="joinTeam" class="jointeam bg-primary text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2 class="section-heading">Are you a professional interested in joining our team....?</h2>
+                    <p>Give us a call on 1800-000-000 and we will meet to discuss how we can work together...</p>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+        <section id="features" class="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="section-heading">
+                        <h2 id="services">Get all of your work done right</h2>
+                        <p class="text-muted">Check out all the services provided by our company</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="device-container">
+                        <div class="device-mockup iphone6_plus portrait white">
+                            <div class="device">
+                                <div class="screen">
+                                    
+                                    <img id="imgservices" src="images/bedroom.jpg" class="img-responsive" alt=""> </div>
+                                <div class="button">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="icon-screen-smartphone text-primary"></i>
+                                    <h3>Discover design ideas</h3>
+                                    <p class="text-muted">Browse more than 1000 photos and filter based on your taste</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="icon-camera text-primary"></i>
+                                    <h3>Find Home Professionals</h3>
+                                    <p class="text-muted">Find local professionals in our team to help you with your project</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="icon-present text-primary"></i>
+                                    <h3>Shop for your home</h3>
+                                    <p class="text-muted">Decorate and design with products from our amazing collection</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="icon-lock-open text-primary"></i>
+                                    <h3>Services</h3>
+                                    <p class="text-muted">Painters<br>Plumbers<br>Electricians<br>HVAC Contractors<br>Renovation</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="icon-present text-primary"></i>
+                                    <h3>Shop for your home</h3>
+                                    <p class="text-muted">Decorate and design with products from our amazing collection</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="icon-lock-open text-primary"></i>
+                                    <h3>Services</h3>
+                                    <p class="text-muted">Painters<br>Plumbers<br>Electricians<br>HVAC Contractors<br>Renovation</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="cta">
+        <div class="cta-content">
+            <div class="container">
+                <h3>Stop waiting.<br>Start building.</h3>
+                <a href="#contact" class="btn btn-outline btn-xl page-scroll">Let's Get Started!</a>
+            </div>
+        </div>
+        <div class="overlay"></div>
+    </section>
+    <section id="contact" class="contact bg-primary">
+        <div class="container">
+            <h2>We <i class="fa fa-heart"></i> new friends!<br>Call us on 1800-000-000 and follow us to see our amazing work</h2>
+            <ul class="list-inline list-social">
+                <li class="social-twitter">
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li class="social-facebook">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li class="social-google-plus">
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2017 All Rights Reserved.</p>
+            <ul class="list-inline">
+                <li>
+                    <a href="#">Privacy</a>
+                </li>
+                <li>
+                    <a href="#">Terms</a>
+                </li>
+                <li>
+                    <a href="#">FAQ</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+
+
+</body>
+
+
+</html>

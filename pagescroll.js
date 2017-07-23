f36de1b04301d37e481e1f3f2e9e0aa67416d723
rosyrambani <rosys9319@gmail.com>
2017-07-22 23:19:50 -0400

    final layout

--- pagescroll.js
+++ pagescroll.js
@@ -11,10 +11,10 @@
     });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 100
-    });
+    // $('body').scrollspy({
+    //     target: '.navbar-fixed-top',
+    //     offset: 100
+    // });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -22,10 +22,10 @@
     });
 
     // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 50
-        }
-    })
+    // $('#mainNav').affix({
+    //     offset: {
+    //         top: 50
+    //     }
+    // })
 
 })(jQuery); // End of use strict
